7ded84d3856c623df53b51adc4db43dc28ad5f40
Jeferson Arango <jeferson.arango@ucaldas.edu.co>
2022-09-05 19:08:11 -0500

    Creación de relaciones en los modelos de bloque, cargo, eleccion, candidato, movimiento, votante y tipo de documento

--- src/models/cargo.model.ts
+++ src/models/cargo.model.ts
@@ -1,4 +1,7 @@
-import {Entity, model, property} from '@loopback/repository';
+import {Entity, hasMany, model, property, belongsTo} from '@loopback/repository';
+import {BloqueCargo} from './bloque-cargo.model';
+import {Bloque} from './bloque.model';
+import {Eleccion} from './eleccion.model';
 
 @model()
 export class Cargo extends Entity {
@@ -20,6 +23,11 @@ export class Cargo extends Entity {
   })
   descripcion?: string;
 
+  @hasMany(() => Bloque, {through: {model: () => BloqueCargo}})
+  bloques: Bloque[];
+
+  @belongsTo(() => Eleccion)
+  eleccionId: number;
 
   constructor(data?: Partial<Cargo>) {
     super(data);
